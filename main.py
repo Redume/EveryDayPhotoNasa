6deb9d20b832e22f25a451581bfa32bc49488bf1
kodiprogdis <aidarxabib33487@gmail.com>
2022-08-01 18:08:02 +0300

    Автоматическое добавление фотографии в приложение.пока одна фотка

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import eel
+import os
 
+eel.init('web', allowed_extensions=['.js', '.html','.css'])
 
-eel.init('web')
-eel.start('html/index.html', size=(800, 600))
+directory = __file__
+print(directory)
+directory = directory[:-7]+"web\images" #получаем путь к папке с фотками
+name_file = os.listdir(directory) #путь к папке
+#for i in name_file:
+	#eel.photo_name(i)  # передаем js'у название файла
+
+print(name_file[1])
+eel.photo_name(name_file[1]) #передаем js'у название файла
+
+eel.start('html/index.html')
